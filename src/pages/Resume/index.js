c821fcdcd0813692a3f5db2b6f5566db8ecbe73f
Teresa Liu <kuas033@gmail.com>
2020-04-17 17:34:47 -0700

    remodel resume page

--- src/pages/Resume/index.js
+++ src/pages/Resume/index.js
@@ -6,9 +6,17 @@ function Resume() {
     return (
         <div>
             <div id="container">
-                <img src={require("../../assets/tree.png")} width="100" height="100" />
+                <img id="tree"src={require("../../assets/treeRoot.png")} alt="Snow" width="100%;" />
+                <li  id="test" class="list-group-item centered">test1</li>             
+                <li  id="test2" class="list-group-item centered">test2</li>
+                <li  id="test3" class="list-group-item centered">test3</li>
+                <li  id="test4" class="list-group-item centered">test4</li>             
+                <li  id="test5" class="list-group-item centered">test5</li>
+                <li  id="test6" class="list-group-item centered">test6</li>
+                
             </div>
         </div>
+
     )
 }
 
